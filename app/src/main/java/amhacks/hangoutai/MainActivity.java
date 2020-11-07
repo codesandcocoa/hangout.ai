9e86f614c7e36e3c6227bfbb356f5ead8288125e
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-11-07 19:27:53 +0530

    Started MainActivity UI

--- app/src/main/java/amhacks/hangoutai/MainActivity.java
+++ app/src/main/java/amhacks/hangoutai/MainActivity.java
@@ -2,13 +2,21 @@ package amhacks.hangoutai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner fSpin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fSpin = (Spinner) findViewById(R.id.firstSpinner);
+
+
     }
 }
